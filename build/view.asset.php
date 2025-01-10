e5fd95f97683d5513cb922a9b04bf62481b8faf8
sohye <sohyek@white64.com>
2025-01-10 06:55:49 -0500

    block settings

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '0ff1e6b9aaed4e28f828');
+<?php return array('dependencies' => array(), 'version' => '5facc5c3b3c3a64b6b6f');
--- build/view.js
+++ build/view.js
@@ -1,1 +1,10 @@
-console.log("Hello World! (from create-block-boilerplate block)");
+/******/ (() => { // webpackBootstrap
+/*!*********************!*\
+  !*** ./src/view.js ***!
+  \*********************/
+/* eslint-disable no-console */
+console.log('Hello World! (from create-block-boilerplate block)');
+/* eslint-enable no-console */
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
